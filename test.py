c5f1340d7f2956d28537aecde2333d5cc62ce8ea
Conrad Borcher <cborcher@fractions01.hcii.cs.cmu.edu>
2023-05-12 19:48:06 -0400

    simple problem selection

--- test.py
+++ test.py
@@ -13,4 +13,16 @@ except ValueError as e:
     sys.stderr.write("Error parsing JSON input: {}\n".format(e))
     sys.exit(1)
 
-print("""{"problem_name":"%s"}""" % input_dict['problems'][0]['name'])
+# Remove seen problems
+input_dict['problems'] = [entry for entry in input_dict['problems'] if entry['completed_count'] == 0]
+
+# Custom selection
+has_mastered_a_skill = sum([entry['p_known'] > .8 for entry in input_dict['skills']]) >= 3
+
+# Default is next problem
+ans = input_dict['problems'][0]['name']
+
+# Once three skills are mastered, show a specific problem
+if has_mastered_a_skill: ans = '1+2(2x-1)=7'
+
+print("""{"problem_name":"%s"}""" % ans)
